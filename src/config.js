7a246428a3f29db7bdfee1912c77863ade38ece5
Shrey123ansh <shreyann56sh@gmail.com>
2024-09-06 00:53:44 +0530

    first commit

---
+++ src/config.js
@@ -0,0 +1,6 @@
+export const NFTCONTRACT = "0xC6Ad2824B03275D4cC5E8f4f61c5a143b999717b";
+export const STAKINGCONTRACT = "0x09aE75315fE2E63271B4F218C4b00F4fF143052A";
+export const polygonscanapi = "https://api-testnet.polygonscan.com/api";
+export const moralisapi = "https://deep-index.moralis.io/api/v2/";
+export const nftpng =
+  "https://ipfs.io/ipfs/QmemFctfTg5uDoCiyptat3ua9n5XSy978EYBDzXS1WrEq5/";
